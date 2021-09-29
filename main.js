88b36cbc9763ded49a23646ee49ae3252aca0584
clartor <clara.tornblom@gmail.com>
2021-09-29 15:58:43 +0200

    menu added mac

--- main.js
+++ main.js
@@ -36,7 +36,38 @@ const isMac = process.platform === "darwin";
 const template = [
   // { role: 'appMenu' }
   ...(isMac
-    ? []
+    ? [
+      {
+        label: app.name,
+        submenu: [
+          { label: "Save File" },
+          { label: "Load File" },
+          { role: "about" },
+          { type: "separator" },
+          { role: "services" },
+          { type: "separator" },
+          { role: "hide" },
+          { role: "hideOthers" },
+          { role: "unhide" },
+          { type: "separator" },
+          { role: "quit" },
+        ],
+      },
+      {
+        label: "View",
+        submenu: [
+          { role: "reload" },
+          { role: "forceReload" },
+          { role: "toggleDevTools" },
+          { type: "separator" },
+          { role: "resetZoom" },
+          { role: "zoomIn" },
+          { role: "zoomOut" },
+          { type: "separator" },
+          { role: "togglefullscreen" },
+        ],
+      },
+    ]
     : [
         {
           label: app.name,
